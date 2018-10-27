351a77eccbbc2f2fa5b1bfcfaad37f483c51b040
Vadim <rmnvich@mail.ru>
2018-10-27 16:37:13 +0300

    first commit

---
+++ app/src/main/java/rmnvich/apps/familybudget/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package rmnvich.apps.familybudget
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
